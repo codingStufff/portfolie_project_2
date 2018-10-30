39209ad3dbef6a60f728199d3242465eeae0fe1d
Dev <prashin@ruc.dk>
2018-10-30 13:51:46 +0100

    Outer facing contract binding setup

--- rawdata_pp_2/rawdata_pp_2/Models/PostModel.cs
+++ rawdata_pp_2/rawdata_pp_2/Models/PostModel.cs
@@ -7,6 +7,16 @@ namespace rawdata_pp_2.Models
 {
     public class PostModel
     {
-
+        public string Url { get; set; }
+        public int TypeId { get; set; }
+        public int? AcceptedAnswerId { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public int Score { get; set; }
+        public string PostText { get; set; }
+        public DateTime? ClosedDate { get; set; }
+        public string Title { get; set; }
+        public string Tags { get; set; }
+        public int AuthorId { get; set; }
+        public string Comment { get; set; }
     }
 }
