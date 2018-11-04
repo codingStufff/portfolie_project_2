46b2832e0ce8458bf0329939cbe99de74e06fbe7
codingStufff <seb_615@hotmail.com>
2018-11-04 18:34:10 +0100

    half way done with implementing the login part

--- rawdata_pp_2/rawdata_pp_2/Controllers/UsersController.cs
+++ rawdata_pp_2/rawdata_pp_2/Controllers/UsersController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DomainModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using rawdata_pp_2.Models;
 using rawdata_pp_2.Service;
 
@@ -17,6 +21,7 @@ namespace rawdata_pp_2.Controllers
         private readonly IDataService _dataService;
         public PasswordService _passwordService = new PasswordService();
         public int PswSize = 256;
+
 
         public UsersController(IDataService dataService)
         {
@@ -37,6 +42,45 @@ namespace rawdata_pp_2.Controllers
             var pwd = PasswordService.HashPassword(model.UserPassword, salt, PswSize);
             _dataService.createUser(pwd, model.UserName, model.Age, model.DisplayName, model.UserLocation, salt);
             return Ok();
+        }
+
+        public IActionResult UserLogin(UserLoginModel model)
+        {
+            string key = "AasdasdaASFF78SDsdasDSADAF";
+
+            if (string.IsNullOrEmpty(model.UserName) || string.IsNullOrEmpty(model.UserPassword))
+            {
+                return BadRequest("Both username and password needs to be filled");
+            }
+            //when we get the configuration file woeking we need to use this if statement
+            /*if(size == 0)
+            {
+                //we fucked something up!
+                return BadRequest();
+            }*/
+
+
+            
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.UTF8.GetBytes(key);
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, model.UserName),
+                }),
+                Expires = DateTime.Now.AddMinutes(30),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescription));
+
+            var response = new
+            {
+                model.UserName,
+                token
+            };
+            return Ok(response);
         }
     }
 }
