e63d13396d04249cae1698d6f335587d65f58ad7
codingStufff <seb_615@hotmail.com>
2018-11-04 12:33:10 +0100

    added UserLoginModel and UserRegistrationModel

---
+++ rawdata_pp_2/rawdata_pp_2/Models/UserRegistrationModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rawdata_pp_2.Models
+{
+    public class UserRegistrationModel
+    {
+    }
+}
