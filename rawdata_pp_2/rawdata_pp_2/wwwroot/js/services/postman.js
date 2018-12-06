f577927a1207abe595549c2aef6301ac584c301c
Dev <prashin@ruc.dk>
2018-12-06 14:02:41 +0100

    portfolio front-end setup

---
+++ rawdata_pp_2/rawdata_pp_2/wwwroot/js/services/postman.js
@@ -0,0 +1,29 @@
+﻿define([], function () {
+    var subscribers = [];
+    var debug = true;
+    var subscribe = function (event, callback) {
+        var subscriber = { event, callback };
+        if (debug) console.log("subscribe: " + JSON.stringify(subscriber));
+
+        subscribers.push(subscriber);
+
+        // return unsubscribe function
+        return function () {
+            subscribers = subscribers.filter(function (e) {
+                return e !== subscriber;
+            });
+        };
+    };
+
+    var publish = function (event, data) {
+        if (debug) console.log("publish: " + JSON.stringify({ event, data }));
+        subscribers.forEach(function (s) {
+            if (event === s.event) s.callback(data);
+        });
+    };
+
+    return {
+        subscribe,
+        publish
+    };
+});
