659889e8f3f1ba76dc9784b3c53e585586fc87df
Dev <prashin@ruc.dk>
2018-12-11 14:00:52 +0100

    login clean up -git conflict

--- rawdata_pp_2/rawdata_pp_2/wwwroot/js/components/login/login.js
+++ rawdata_pp_2/rawdata_pp_2/wwwroot/js/components/login/login.js
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
-﻿define(['knockout', 'dataService'], function (ko, ds) {
-    return function () {
-=======
-﻿define(['knockout', /*'dataService',*/ 'postman'], function (ko, /*ds,*/ postman) {
+define(['knockout', /*'dataService',*/ 'postman'], function (ko, /*ds,*/ postman) {
     return function (params) 
     {
->>>>>>> f2771f542131f92ed5671abfdfe1a8964e97de46
         var username = ko.observable();
         var password = ko.observable();
         var errorMsg = ko.observable();
 
-<<<<<<< HEAD
         var doLogin = function () {
             errorMsg("");
             var loginInfo = { "userName": username(), "userPassword": password() };
@@ -23,37 +17,23 @@
                     errorMsg("Wrong username or password");
                 });
         }
-=======
+
         var login = function () 
         {
             console.log(username()+' '+password());
 
         };
 
-         var goToLogin = function() {
-            postman.publish("changeMenu", "login");
-        };
->>>>>>> f2771f542131f92ed5671abfdfe1a8964e97de46
-
         var goToLogin = function () {
             postman.publish("changeMenu", "login");
         };
 
-<<<<<<< HEAD
         return {
             doLogin,
             username,
             password,
             errorMsg, 
             goToLogin
-=======
-        return{
-            login,
-            username,
-            password,
-            goToLogin
-
->>>>>>> f2771f542131f92ed5671abfdfe1a8964e97de46
         };
 
     };
